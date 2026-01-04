5bdd05c6a6a410b055d434c6b68e8896a09d9f80
Anderson Blanco <andersonblanco@Andersons-MacBook-Air.local>
2026-01-03 20:25:34 -0500

    removed exposed commented secrets

--- src/utils/parseAndTransferToFirestore/transferCSVToFirebase.js
+++ src/utils/parseAndTransferToFirestore/transferCSVToFirebase.js
@@ -13,14 +13,7 @@ const firebaseConfig = {
 };
 
 /*
-Netlify Enviornmental Variables: 
-VITE_FIREBASE_API_KEY = AIzaSyAc836mzZfYSXOXKxr6agghoDoZhISLNrU
-VITE_FIREBASE_PROJECT_ID = delirio-480110
-VITE_FIREBASE_APP_ID = 1:497066549778:web:e98be0a287edfc5ba92ecb
-VITE_FIREBASE_AUTH_DOMAIN = delirio-480110.firebaseapp.com
-VITE_FIREBASE_STORAGE_BUCKET = delirio-480110.firebasestorage.app
-VITE_FIREBASE_MESSAGING_SENDER_ID = 497066549778
-VITE_FIREBASE_MEASUREMENT_ID = G-HJ752JWYKT
+
 */
 
 // Initialize Firebase
